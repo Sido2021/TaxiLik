e19790024ee55022c181de826dfaf1f56d0ab78a
Yassine <merouaneyassin@gmail.com>
2021-01-12 02:36:13 +0100

    test profile

--- app/src/main/java/com/taxilik/client/profile/ClientProfileFragment.java
+++ app/src/main/java/com/taxilik/client/profile/ClientProfileFragment.java
@@ -1,38 +1,110 @@
 package com.taxilik.client.profile;
 
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
 import com.taxilik.R;
 
-public class ClientProfileFragment extends Fragment {
+import org.json.JSONObject;
 
-    private ClientProfileViewModel mViewModel;
+public class ClientProfileFragment extends AppCompatActivity {
+    String URL_host = "https://taxiliktestv.000webhostapp.com/profile.php";
+    String profile_id="tp3364ecdcg1runtn0t04jsd8d8smvsrb68xb8rzu0ktudzsbk6aygoouley2fp4p7otqefv7o28ten5gy9qnffgs302wt979qtsy0mlci6ss6tf36mg2mf1tr4oq7r7z0us9bremve0oi2ci8cqy50oqrq2pby7d5u26rqjlg6r5zv9g1tgg5tpeyu9fbirzfc33jlwox71galbu2qdwe7rby1vjla771mjiqenetd7k7tjiqzfds3mik";
+    String URL_Profile = URL_host+"?id="+profile_id;
+    TextView fullname_field,username_field,fname_prof,lname_prof,email_prof,phone_prof;
+    ;
+    Button edit_profile;
 
-    public static ClientProfileFragment newInstance() {
-        return new ClientProfileFragment();
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.client_profile_fragment, container, false);
-    }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ClientProfileViewModel.class);
-        // TODO: Use the ViewModel
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_profile_fragment);
+        fullname_field=findViewById(R.id.fullname_field);
+        username_field=findViewById(R.id.username_field);
+        fname_prof=findViewById(R.id.fname_prof);
+        lname_prof=findViewById(R.id.lname_prof);
+        email_prof=findViewById(R.id.email_prof);
+        phone_prof=findViewById(R.id.phone_prof);
+//        fullname_field=findViewById(R.id.fullname_field);
+//        username_field=findViewById(R.id.username_field);
+//        full_name_profile=findViewById(R.id.full_name_profile);
+//        email_profile=findViewById(R.id.email_profile);
+//        phone_profile=findViewById(R.id.phone_profile);
+//        password_profile=findViewById(R.id.password_profile);
+        edit_profile=findViewById(R.id.edit_profile);
+        edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intant=new Intent(ClientProfileFragment.this,ClientProfileFragmentEdit.class);
+                startActivity(intant);
+                finish();
+                return;
+            }
+        });
+        profile();
+
+    }
+
+    private void setEditText(String nom,String prenom,String email,String phone){
+        fullname_field.setText(nom+" "+prenom);
+        username_field.setText(nom+"_"+prenom+"@");
+        fname_prof.setText(nom);
+        lname_prof.setText(prenom);
+        email_prof.setText(email);
+        phone_prof.setText(phone);
+    }
+    private void profile()
+    {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_Profile,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("anyText",response);
+                        try{
+                            //1-cas d'une base de donnee
+//                            JSONObject jsonObject = new JSONObject(response);
+//                            String id = jsonObject.getString("id");
+//                            String nom = jsonObject.getString("nom");
+//                            String prenom = jsonObject.getString("prenom");
+//                            String email=jsonObject.getString("email");
+//                            String phone=jsonObject.getString("phone");
+////                            setEditText(nom,prenom,email,phone);
+//                            Toast.makeText(ClientProfileFragment.this, nom+" - "+prenom, Toast.LENGTH_SHORT).show();
+
+                            //2-pour tester only
+                             setEditText("YASSINE","MEROUANE","yassinemer05@gmail.com","+212 643427501");
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),"profile error 1-"+e,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"profile error 2-"+error,Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 }
---
+++ app/src/main/java/com/taxilik/client/profile/ClientProfileFragmentEdit.java
@@ -0,0 +1,15 @@
+package com.taxilik.client.profile;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.taxilik.R;
+
+public class ClientProfileFragmentEdit extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_profile_edit_fragment);
+}}
